8512ec852b9cba86c96b3373f88133100a3120b1
Anthony Maley <tony@maley.be>
2026-04-05 19:20:16 -0400

    chore: version bump to 1.6.1

--- tools/music/Sources/Music.swift
+++ tools/music/Sources/Music.swift
@@ -5,7 +5,7 @@ struct Music: ParsableCommand {
     static let configuration = CommandConfiguration(
         commandName: "music",
         abstract: "Control Apple Music from the terminal.",
-        version: "1.6.0",
+        version: "1.6.1",
         subcommands: [
             // Playback
             Play.self,
